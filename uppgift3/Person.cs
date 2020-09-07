cab7fb8d2e13af1ab74e4190fb62535b8b2c04d5
ulfsv <ulf.e.sv@gmail.com>
2020-09-07 02:03:30 +0200

    Add project files.

---
+++ uppgift3/Person.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+
+namespace uppgift3
+{
+    internal class Person
+    {
+
+
+        private int age;
+        private string fName;
+        private string lName;
+        private double height;
+        private double weight;
+
+
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Ålder kan inte vara negativt.");
+                }
+                age = value;
+            }
+        }
+
+        //FName är obligatorisk och får inte vara mindre än 
+        //2 tecken eller längre än 10 tecken
+        public string FName
+        {
+            get
+            {
+                return fName;
+            }
+            set
+            {
+                if (value.Length < 2 || value.Length > 10)
+                {
+                    throw new ArgumentException($"Förnamnet ska vara minst två tecken. Men ej längre än 10 tecken.");
+                }
+                fName = value;
+            }
+
+        }
+        //LName är obligatorisk och får inte vara mindre än 3 tecken eller större än 15
+        //tecken.
+
+        public string LName
+        {
+            get
+            { return lName; }
+            set
+            {
+                if (value.Length < 3 || value.Length > 15)
+                {
+                    throw new ArgumentException($"Efternamnet ska vara 3 till 15 tecken.");
+                }
+                lName = value;
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        public Person(int age, string fName, string lName, double height, double weight)
+        {
+            Age = age;
+            FName = fName;
+            LName = lName;
+            Height = height;
+            Weight = weight;
+        }
+        public override string ToString()
+        {
+            return $"Name: {FName} {LName} Age: {Age} Height: {height} Weight: {weight}";
+        }
+    }
+}
---
+++ uppgift3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace uppgift3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3.1
+            try
+            {
+                //Person person1 = new Person(20, "Pelle", "Väldigtlångtefternamne", 205, 90);
+                //Console.WriteLine(person1.ToString());
+                //Person person2 = new Person(-1, "Pelle", "Pettersson", 130, 90);
+                //Console.WriteLine(person2.ToString());
+                Person person3 = new Person(20, "U", "Fritsson", 205, 90);
+                Console.WriteLine(person3.ToString());
+            }
+            catch (ArgumentException ex) //Se till att hantera undantagen i Program-klassen med en try-catch block.
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
